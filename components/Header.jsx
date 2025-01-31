b3b485a6eabe528c11c4a0f772c84a821a77fe1b
henryprosser <henryprosser@yahoo.co.uk>
2025-01-31 09:42:54 +0000

    Implement header and logo

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        src="../images/chef-claude-icon.png"
+        className="header__icon"
+        alt="chef claude"
+      />
+      <h1 className="header__title">Chef Claude</h1>
+    </header>
+  );
+}
