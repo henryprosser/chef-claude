7fdb1f4822aa1217c8a0e8a831e2c12e63469944
henryprosser <henryprosser@yahoo.co.uk>
2025-01-31 10:27:58 +0000

    Create add ingredient form and button

--- App.jsx
+++ App.jsx
@@ -1,5 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
