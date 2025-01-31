667f480a94d6cad1b66aa952ef5406893a0ce402
henryprosser <henryprosser@yahoo.co.uk>
2025-01-31 14:29:49 +0000

    Enable ingredient addition functionality

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
   return (
     <main>
-      <form className="ingredient__form">
+      <form onSubmit={handleSubmit} className="ingredient__form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
           className="ingredient__input"
+          name="ingredient"
         />
         <button className="ingredient__button">Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
