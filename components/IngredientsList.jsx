eb4e2232032e4fb08ddd1fa0040ae52e651909c4
henryprosser <henryprosser@yahoo.co.uk>
2025-02-03 10:24:33 +0000

    Refactor code by adding Recipe and IngredientsList components

---
+++ components/IngredientsList.jsx
@@ -0,0 +1,23 @@
+export default function IngredientsList(props) {
+  const ingredientsListItems = props.ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  return (
+    <section>
+      <h2 className="ingredients__onhand">Ingredients on hand:</h2>
+      <ul className="ingredients__list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+      {props.ingredients.length > 3 && (
+        <div className="get__recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
